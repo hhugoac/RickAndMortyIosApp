1cc62a8aabbf5be9325cfd2f7abf218cdaaf8007
Hugo Alonzo <hhugoac@github.com>
2024-06-23 18:51:50 -0600

    Initial Commit

---
+++ RickAndMortyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyApp
+//
+//  Created by Hugo Alonzo on 23/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
