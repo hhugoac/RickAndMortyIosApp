ab7b323f9956145cd82451ae61d4497aae865dd2
hectoralonzofg <hector_alonzo@fragua.com.mx>
2024-06-27 14:47:44 -0600

    Crating the data structs for the API rest services

---
+++ RickAndMortyApp/Models/RMCharacterStatus.swift
@@ -0,0 +1,14 @@
+//
+//  RMCharacterStatus.swift
+//  RickAndMortyApp
+//
+//  Created by Hector Alonzo  on 27/06/24.
+//
+
+import Foundation
+
+enum RMCharacterStatus: String, Codable {
+    case alive = "Alive"
+    case dead = "Dead"
+    case `unknown` = "unknown"
+}
---
+++ RickAndMortyApp/Models/RMSingleLocation.swift
@@ -0,0 +1,13 @@
+//
+//  RMSingleLocation.swift
+//  RickAndMortyApp
+//
+//  Created by Hector Alonzo  on 27/06/24.
+//
+
+import Foundation
+
+struct RMSingleLocation: Codable {
+    let name:String
+    let url:String
+}
