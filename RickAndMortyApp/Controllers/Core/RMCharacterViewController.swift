03bcb312b295935a4ffb25c2614736d31c7bb4c7
Hugo Alonzo <hhugoac@github.com>
2024-06-29 20:49:29 -0600

    In this commit contains the basic UI for the collectionview

--- RickAndMortyApp/Controllers/Core/RMCharacterViewController.swift
+++ RickAndMortyApp/Controllers/Core/RMCharacterViewController.swift
@@ -10,22 +10,23 @@ import UIKit
 /// Controller to Character
 final class RMCharacterViewController: UIViewController {
 
+    private let rmCharacteher = RMCharacterListView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
         self.title =  "Characters"
-        
-        RMService.shared.execute(.listCharactersRequest, expecting: RMGetAllCharactersResponse.self) { result in
-            switch result {
-            case .success(let model):
-                print("SUCCESS: " + String(describing: model))
-            case .failure(let error):
-                print("FAILURE: " + String(describing: error))
-            }
-            
-        }
+        view.addSubview(rmCharacteher)
+        setupView()
     }
     
-
+    private func setupView() {
+        NSLayoutConstraint.activate([
+            rmCharacteher.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            rmCharacteher.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            rmCharacteher.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            rmCharacteher.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor)
+        ])
+    }
 }
