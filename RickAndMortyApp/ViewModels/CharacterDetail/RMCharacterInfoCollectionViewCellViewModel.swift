6728a837344058d4840e5feb85b2b35e1e774c52
hectoralonzofg <hector_alonzo@fragua.com.mx>
2024-07-23 17:36:43 -0600

    feat: INFO CELL VIEWS
    Display the info in each cell with the viewmodel data.
     Changes to be committed:
    	modified:   RickAndMortyApp/ViewModels/CharacterDetail/RMCharacterInfoCollectionViewCellViewModel.swift
    	modified:   RickAndMortyApp/Views/CharacterDetails/RMCharacterInfoCollectionViewCell.swift

--- RickAndMortyApp/ViewModels/CharacterDetail/RMCharacterInfoCollectionViewCellViewModel.swift
+++ RickAndMortyApp/ViewModels/CharacterDetail/RMCharacterInfoCollectionViewCellViewModel.swift
@@ -9,12 +9,49 @@ import Foundation
 
 final class RMCharacterInfoCollectionViewCellViewModel {
     
+    private let type:`Type`
     public let value: String
-    public let title: String
+    public let title: String {
+        self.type.displayTitle
+    }
     
-   init(value: String, title: String) {
+    enum `Type` {
+        case status
+        case gender
+        case type
+        case species
+        case origin
+        case location
+        case created
+        case total
+        
+        var displayTitle: String {
+            switch self {
+                case .status:
+                    return "Something"
+                case .gender:
+                    return "Something"
+                case .type:
+                    return "Something"
+                case .species:
+                    return "Something"
+                case .origin:
+                    return "Something"
+                case .location:
+                    return "Something"
+                case .created:
+                    return "Something"
+                case .total:
+                    return "Something"
+                default:
+                    <#code#>
+            }
+        }
+    }
+    
+    init(type: `Type`, value: String) {
+        self.type = type
             self.value = value
-            self.title = title
     }
     
 }
