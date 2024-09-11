0f00bfb5a0a57a8be2ac1ad94de225b3de5beed2
Hugo Alonzo <hhugoac@github.com>
2024-09-10 22:48:38 -0600

    gg

--- RickAndMortyApp/Controllers/Core/RMEpisodeViewController.swift
+++ RickAndMortyApp/Controllers/Core/RMEpisodeViewController.swift
@@ -9,12 +9,32 @@ import UIKit
 
 final class RMEpisodeViewController: UIViewController {
 
+    private let rmCharacterListView = RMCharacterListView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
-        self.title = "Episodes"
+        self.title =  "Characters"
+        view.addSubview(rmCharacterListView)
+        setupView()
     }
     
+    private func setupView() {
+        rmCharacterListView.delegate = self
+        NSLayoutConstraint.activate([
+            rmCharacterListView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            rmCharacterListView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            rmCharacterListView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            rmCharacterListView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor)
+        ])
+    }
 
+    // MARK: - RMCharacterLisViewDelegate
+    func rmCharacterListView(_ characterListView: RMCharacterListView, didSelectCharacter character: RMCharacter) {
+        let viewModel = RMCharacterDetailViewViewModel(character: character)
+        let detailVC = RMCharacterDetailViewController(viewModel: viewModel)
+        detailVC.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(detailVC, animated: true)
+    }
 }
