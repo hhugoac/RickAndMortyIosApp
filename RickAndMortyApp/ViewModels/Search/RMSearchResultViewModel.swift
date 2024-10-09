42d2323b4d8ee7d7d9511510664f5ece1dbb2a2a
Hugo Alonzo <hhugoac@github.com>
2024-10-08 22:58:35 -0600

    Search more results

--- RickAndMortyApp/ViewModels/Search/RMSearchResultViewModel.swift
+++ RickAndMortyApp/ViewModels/Search/RMSearchResultViewModel.swift
@@ -76,6 +76,57 @@ final class RMSearchResultViewModel {
             
         }
     }
+    
+    func fetchAdditionalResults(completion: @escaping ([RMLocationTableViewCellViewModel])->Void) {
+        guard !isLoadingMoreResults else {
+            return
+        }
+        
+        guard let nextUrlString =  next,
+              let url = URL(string: nextUrlString) else {
+            return
+        }
+        
+        isLoadingMoreResults = true
+        
+        guard let request = RMRequest(url:url) else {
+            isLoadingMoreResults = false
+            return
+        }
+        switch results {
+        case .characters(let existingResults)
+            RMService.shared.execute(request, expecting: RMGetAllCharactersResponse.self) { [weak self] result in
+                guard let strongSelf = self else {
+                    return
+                }
+                switch result {
+                case .success(let responseModel):
+                    let moreResults = responseModel.results
+                    let info = responseModel.info
+                    strongSelf.next = info.next
+                    
+                    let additionalResults = moreResults.compactMap({
+                        return RMCharacterCollectionViewCellViewModel(characterName: $0.name,
+                                                                      characterStatus: $0.status,
+                                                                      characterImageUrl: URL(string: $0.image))
+                    })
+                    var newResults: [RMCharacterCollectionViewCellViewModel] = []
+                    newResults = existingResults + additionalResults
+                    strongSelf.results = .characters(newResults)
+                    DispatchQueue.main.async {
+                        strongSelf.isLoadingMoreResults = false
+                        completion(newResults)
+                    }
+                    
+                case .failure(let failure):
+                    print(String(describing: failure))
+                    self?.isLoadingMoreResults = false
+                }
+                
+                
+            }
+        }
+    }
 }
 
 enum RMSearchResultType {
--- RickAndMortyApp/Views/Search/RMSearchResultsView.swift
+++ RickAndMortyApp/Views/Search/RMSearchResultsView.swift
@@ -229,6 +229,28 @@ extension RMSearchResultsView: UICollectionViewDataSource,
         return CGSize(width: width, height: 100)
 
     }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        guard kind == UICollectionView.elementKindSectionFooter,
+              let footer = collectionView.dequeueReusableSupplementaryView(
+                ofKind: kind,
+                withReuseIdentifier: RMFooterLoadingCollectionReusableView.identifier,
+                for: indexPath) as? RMFooterLoadingCollectionReusableView else
+        { fatalError("Unsupported") }
+        if let viewModel = viewModel, viewModel.shouldShowLoadMoreIndicator {
+            footer.startAnimating()
+        }
+        return footer
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+            guard let viewModel = viewModel,
+                  viewModel.shouldShowLoadMoreIndicator else {
+                return .zero
+            }
+            return CGSize(width: collectionView.frame.width,
+                          height: 100)
+        }
 }
 
 extension RMSearchResultsView: UIScrollViewDelegate {
@@ -241,7 +263,26 @@ extension RMSearchResultsView: UIScrollViewDelegate {
     }
 
     private func handleCharacterOrEpisodePagination(scrollView: UIScrollView) {
-
+        guard let viewModel = viewModel,
+        collectionViewModelCellViewModels.isEmpty,
+        viewModel.shouldShowLoadMoreIndicator,
+        viewModel.isLoadingMoreResults else { return
+        }
+        
+        Timer.scheduledTimer(withTimeInterval: 0.2, repeats: false) { [weak self] t in
+            let offset = scrollView.contentOffset.y
+            let totalContentHeight = scrollView.contentSize.height
+            let totalScrollViewFixedHeight = scrollView.frame.size.height
+            
+            if offset >= (totalContentHeight - totalScrollViewFixedHeight - 120) {
+                viewModel.fetchAdditionalLocation { [weak self] newResults in
+                    self?.tableView.tableFooterView = nil
+                    self?.collectionViewModelCellViewModels = newResults
+                    
+                }
+            }
+            t.invalidate()
+        }
     }
 
     private func handleLocationPagination(scrollView: UIScrollView) {
@@ -260,7 +301,7 @@ extension RMSearchResultsView: UIScrollViewDelegate {
         
             if offset >= (totalContentHeight - totalScrollViewFixedHeight - 100) {
                 DispatchQueue.main.async {
-                    self?.showLoadingIndicator()
+                    self?.showTableLoadingIndicator()                    
                 }
                 viewModel.fetchAdditionalLocation { [weak self] newResults in
                     self?.tableView.tableFooterView = nil
@@ -273,7 +314,7 @@ extension RMSearchResultsView: UIScrollViewDelegate {
         
     }
     
-    private func showLoadingIndicator() {
+    private func showTableLoadingIndicator() {
         let footer = RMTableLoadingFooterView(frame: CGRect(x: 0, y: 0, width: frame.size.width, height: 100))
         tableView.tableFooterView = footer
     }
